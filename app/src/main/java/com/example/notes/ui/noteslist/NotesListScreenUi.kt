e4ded9d6817a4f2823f3acfba6e7e85e7f7e9d38
Kacper <kacperpawlicki04@gmail.com>
2025-08-26 22:33:09 +0200

    Wstepny wyglad glownego ekranu aplikacji.

--- app/src/main/java/com/example/notes/ui/noteslist/NotesListScreenUi.kt
+++ app/src/main/java/com/example/notes/ui/noteslist/NotesListScreenUi.kt
@@ -1,15 +1,45 @@
 package com.example.notes.ui.noteslist
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.notes.data.Note
+import java.time.format.DateTimeFormatter
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NotesListScreenUi(
     modifier: Modifier = Modifier,
@@ -18,20 +48,103 @@ fun NotesListScreenUi(
 ) {
     val notes by viewModel.notes.collectAsStateWithLifecycle()
 
-    LazyColumn(
-        modifier = modifier
-    ){
-        items(notes) { note ->
-            Row(
-                modifier = Modifier
-                    .clickable(
-                        onClick = { onNoteClick(note.id) }
+    val listState = rememberLazyGridState()
+    val fabVisible by remember {
+        derivedStateOf {
+            !listState.isScrollInProgress
+        }
+    }
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Moje notatki",
+                        fontWeight = FontWeight.SemiBold
                     )
+                },
+                actions = {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Wyszukaj notatkę"
+                        )
+                    }
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.MoreVert,
+                            contentDescription = "Więcej opcji"
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            AnimatedVisibility(
+                visible = fabVisible,
+                enter = fadeIn(),
+                exit = fadeOut()
             ) {
-                Text(
-                    text = note.title
-                )
+                FloatingActionButton(
+                    onClick = {},
+
+                    ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Utwórz nową notatkę"
+                    )
+                }
+            }
+
+        }
+    ) { innerPadding ->
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier
+                .padding(innerPadding),
+            contentPadding = PaddingValues(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            state = listState
+        ) {
+            items(notes) { note ->
+                NoteItem(note, onNoteClick)
             }
         }
+    }
+}
+
+@Composable
+fun NoteItem(
+    note: Note,
+    onNoteClick: (Int) -> Unit
+){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(225.dp)
+                .clickable { onNoteClick(note.id) }
+        ) {
+            Text(
+                text = note.content,
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Text(
+            text = "Notatka tekstowa z dnia ${note.creationDate.dayOfMonth}." +
+                    "${note.creationDate.format(DateTimeFormatter.ofPattern("MM"))}",
+            textAlign = TextAlign.Center
+        )
     }
 }
